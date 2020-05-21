230db64f79c072b6003b20f9f385fa71a98a4351
Timothy Ha <timothyha@gmail.com>
2020-05-21 23:17:40 +1200

    Issue #1 options box

--- content.js
+++ content.js
@@ -8,15 +8,31 @@ function setchatbox() {
     return;
   }
 
+  var ison    = true;
+  var isboth  = true;
+  var isfront = true;
+
+  chrome.storage.sync.get({
+    ison: true,
+    isboth: true,
+    isfront: true
+  }, function(items) {
+    ison    = items.ison;
+    isboth  = items.isboth;
+    isfront = items.isfront;
+  });
+
   chatbox.onchange = function () { 
+
+    if( !ison ) return;
 
     var movenum = document.querySelectorAll("#main-wrap > main > div.round__app.variant-standard > div.rmoves > div.moves > index").length;
     if( (movenum == undefined) || (movenum < 1) ) return;
     
     if( chatbox.value.substring(0,2) == "/w" )
-      chatbox.value = '/w (' + movenum + ')' + chatbox.value.substring(2,200); 
-    else
-      chatbox.value = '(' + movenum + ') ' + chatbox.value; 
+      chatbox.value = isfront ? ('/w (' + movenum + ')' + chatbox.value.substring(2,200)) : ('/w' + chatbox.value.substring(2,200) + ' (' + movenum + ')'); 
+    else if ( isboth )
+      chatbox.value = isfront ? ('(' + movenum + ') ' + chatbox.value) : (chatbox.value + ' (' + movenum + ')'); 
   };
 
   clearTimeout(chatboxtimer);
